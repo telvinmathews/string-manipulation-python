a0f2a301e4ebffe8450342807c3872166f4fb8b6
telvinmathews <57323861+telvinmathews@users.noreply.github.com>
2021-11-22 19:25:18 -0600

    created input method

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #Fix the code below 👇
 
-print("Day 1 - String Manipulation")
-print('String Concatenation is done with the "+" sign.')
-print('e.g. print("Hello " + "world")')
-print("New lines can be created with a backslash and n.")
+# print("Day 1 - String Manipulation")
+# print('String Concatenation is done with the "+" sign.')
+# print('e.g. print("Hello " + "world")')
+# print("New lines can be created with a backslash and n.")
+
+print("Hello " + input("What is your name?  "))
